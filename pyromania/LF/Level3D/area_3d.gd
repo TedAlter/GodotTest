d7c7bc99597e11dfe47ded2bc7a79c6367ff1900
TedAlter <ilya.bandysh@gmail.com>
2025-02-05 13:52:21 +0200

    Тест уповільнення
    Спробував реалізувати наложення ефекту уповільнення. Для цього переробив логіку об'єкту: де раніше використовувався SPEED використовується новий параметр cSPEED. На початку виконання він стає рівним до SPEED , але при наложенні уповільнення буде модифікуватися cSPEED . Інформація про початкову (звичайну) швидкість все ще зберігається за параметром SPEED

--- pyromania/LF/Level3D/area_3d.gd
+++ pyromania/LF/Level3D/area_3d.gd
@@ -6,7 +6,6 @@ func _physics_process(delta: float) -> void:
 	for body in bodies:
 		if body is RigidBody3D and body.name == "Player":
 			var cloned_timer = timer.duplicate()  # Клонируем таймер
-			cloned_timer.one_shot = true  # Устанавливаем, чтобы таймер не повторялся
 			body.add_child(cloned_timer)  # Добавляем к объекту
 		# Запускаем таймер
 			cloned_timer.start()
